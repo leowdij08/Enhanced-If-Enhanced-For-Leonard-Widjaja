d46dd6eb8d740aa6623a6f88d229cfde32c6b694
leowdij08 <158173182+leowdij08@users.noreply.github.com>
2024-03-26 21:17:23 +0800

    enhancedfor.java

--- OOP Week 5_Leonard Widjaja_0806022310008/enhancedfor.java
+++ OOP Week 5_Leonard Widjaja_0806022310008/enhancedfor.java
@@ -4,7 +4,7 @@ public class enhancedfor {
                    {4, 3, 7},
                    {7, 2, 9} };
 
-        // Menggunakan enhanced for loop untuk mencetak setiap elemen dalam array of arrays
+        // Menggunakan enhanced for loop untuk mencetak setiap elemen dalam array matrix
         for (int[] row : matrix) { // Mengambil setiap array (baris) dalam matrix
         for (int num : row) {  // Mengambil setiap elemen dalam array (baris) tersebut
         System.out.print(num + " "); // Mencetak elemen
